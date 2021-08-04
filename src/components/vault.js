4e5cf688ee71e60c87c3562b459e91421370f921
Batudo <batudo@2pi.finance>
2021-08-04 14:13:05 -0300

    Experimental alert on vault component

--- src/components/vault.js
+++ src/components/vault.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import Experimental from './experimental'
 import Header from './header'
 import Footer from './footer'
 import Toasts from './toasts'
@@ -7,6 +8,7 @@ import VaultData from './vaultData'
 const Vault = props => {
   return (
     <div className="container">
+      <Experimental />
       <Header path=".." />
       <VaultData id={props.id} />
       <Toasts />
