c7639a25ca17b67c4774eb504e593b876e7576d0
Matías A. Gagliano <matias.gagliano@gmail.com>
2021-09-19 11:48:10 -0300

    Fix redirection loop in 404 page.

--- pages/404.js
+++ pages/404.js
@@ -1,37 +1,81 @@
-import Back from '../src/components/back'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { getClientBuildManifest } from 'next/dist/client/route-loader'
+import { removePathTrailingSlash } from 'next/dist/client/normalize-trailing-slash'
+import { getRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex'
+import Head from 'next/head'
+import Link from 'next/link'
+import Experimental from '../src/components/experimental'
+import Header from '../src/components/header'
+import Toasts from '../src/components/toasts'
+import Footer from '../src/components/footer'
+import Back from '../src/components/back'
 
-const Custom404 = () => {
+const cleanPath = path => {
+  return removePathTrailingSlash(path).replace(/\.html$/, '')
+}
+
+const validatePath = async path => {
+  const manifest = await getClientBuildManifest()
+  const pages    = manifest.sortedPages.filter(page => !page.startsWith('/_'))
+  const isValid  = pages.some(page => getRouteRegex(page).re.test(path))
+
+  if (! isValid) throw new Error('Invalid path')
+}
+
+const NotFoundPage = () => {
   const router = useRouter()
 
   useEffect(() => {
-    if (router.asPath !== '/404') {
-      router.push(router.asPath)
-    }
+    const path = cleanPath(router.asPath)
+
+    // Specifically requested this page
+    if (path === '/404') return
+
+    // If the path matches a route (is valid), redirect to it.
+    // If not, just continue showing this error page.
+    validatePath(path).then(() => router.push(path))
   }, [router])
 
-  const render = () => {
-    if (router.asPath === '/404') {
-      return (
-        <div className="container my-5">
-          <Back />
+  return (
+    <div className="container">
+      <Head>
+        <title>2pi - Not found</title>
+      </Head>
 
-          <h1 className="display-1 text-center my-5 pt-5">
-            Page not found!
-          </h1>
-        </div>
-      )
-    } else {
-      return (
-        <div className="spinner-border text-primary mt-5" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      )
-    }
-  }
-
-  return render()
+      <Experimental />
+      <Header />
+      <NotFound />
+      <Toasts />
+      <Footer />
+    </div>
+  )
 }
 
-export default Custom404
+const NotFound = () => {
+  return (
+    <div className="card my-4 shadow-none">
+      <div className="card-body text-center bg-blur border border-primary border-2 rounded px-lg-5 py-lg-4">
+        <div className="text-start mb-0">
+          <Back />
+        </div>
+
+        <h2 className="h1 text-primary mt-4 mb-0">
+          Not found
+        </h2>
+
+        <p className="text-center lead my-4">
+          The page that you are looking for doesn’t exist.
+        </p>
+
+        <Link href="/">
+          <a className="btn btn-outline-primary bg-dark text-primary fw-bold my-4">
+            Go home
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
